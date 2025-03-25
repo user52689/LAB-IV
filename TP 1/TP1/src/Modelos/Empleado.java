e6dd7d9c1cab32014e28382efa822b16b02b2ed9
Abraham <AbrahamMeza@gmail.com>
2025-03-25 08:45:43 -0300

    add Metodos get y Set clase Empleado

--- TP 1/TP1/src/Modelos/Empleado.java
+++ TP 1/TP1/src/Modelos/Empleado.java
@@ -7,3 +7,36 @@ public class Empleado {
 	private int edad;	
 }
 
+    // Constructor
+    public Empleado(int id, String nombre, int edad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
